cc0fdc338f8078a3b98999c81596cc3e513f1754
AbhishekSingh <147810087+abhishek23me@users.noreply.github.com>
2024-10-10 17:09:46 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+export default {
+    server: {
+      host: '0.0.0.0',  // Exposes the server to the network
+      port: 5173,       // Use the default Vite port
+    }
+  }
+  
